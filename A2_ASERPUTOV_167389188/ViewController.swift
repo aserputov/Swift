1e42a5ba25261ddab33765ae3e5bbf56a345feb9
Anatoliy Serputov <aserputov@myseneca.ca>
2021-10-15 02:15:56 -0400

    Initial Commit

---
+++ A2_ASERPUTOV_167389188/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A2_ASERPUTOV_167389188
+//
+//  Created by Anatoliy Serputov on 2021-10-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
